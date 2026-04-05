6562f0f4161d39910078542e1739031d5d355026
Eduardo Rojas <eduardorojas@example.com>
2026-04-05 00:49:28 -0300

    deploy ajustado

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./pages/Home.vue";
-import Login from "./pages/Login.vue";
-import Signup from "./pages/Signup.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import("./pages/Home.vue"),
   },
   {
     path: "/login",
     name: "Login",
-    component: Login,
+    component: () => import("./pages/Login.vue"),
   },
   {
     path: "/signup",
     name: "Signup",
-    component: Signup,
+    component: () => import("./pages/Signup.vue"),
   },
 ];
 
